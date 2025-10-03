7923c61fc5cf3118804162df52dd1af90e1f757e
CrossDarkrix <crosseddarkrix@gmail.com>
2025-10-03 18:07:25 +0900

    Update CaptureBoardViewer.py

--- CaptureBoardViewer.py
+++ CaptureBoardViewer.py
@@ -4,7 +4,6 @@ from PySide6.QtCore import Qt, Signal, Slot, QThread, QSize
 from PySide6.QtWidgets import QMainWindow, QLabel, QApplication
 from PySide6.QtGui import QImage, QPixmap
 
-
 class VideoThread(QThread):
     change_pixmap_signal = Signal(QImage)
     playing = True
@@ -16,8 +15,7 @@ class VideoThread(QThread):
             if ret:
                 h, w, ch = frame.shape
                 bytesPerLine = ch * w
-                image = QImage(frame, w, h, bytesPerLine, QImage.Format.Format_BGR888).scaled(1270, 720, Qt.AspectRatioMode.IgnoreAspectRatio, Qt.TransformationMode.SmoothTransformation)
-                self.change_pixmap_signal.emit(image)
+                self.change_pixmap_signal.emit(QImage(frame, w, h, bytesPerLine, QImage.Format.Format_BGR888).scaled(QSize(1920, 1080), Qt.AspectRatioMode.IgnoreAspectRatio, Qt.TransformationMode.SmoothTransformation))
         cap.release()
 
     def stop(self):
@@ -25,9 +23,7 @@ class VideoThread(QThread):
         self.wait()
 
 class Window(QMainWindow):
-
-    video_size = QSize(1270, 720)
-
+    video_size = QSize(1280, 768)
     def __init__(self):
         super().__init__()    
         self.initUI()
@@ -35,6 +31,9 @@ class Window(QMainWindow):
         self.thread = VideoThread()
         self.thread.change_pixmap_signal.connect(self.update_image)
         self.thread.start()
+
+    def closeEvent(self, _):
+        self.thread.stop()
 
     def initUI(self):
         self.setFixedSize(self.video_size)
@@ -45,10 +44,10 @@ class Window(QMainWindow):
         if e.key() == Qt.Key.Key_Q:
             self.thread.stop()
             sys.exit()
-    
+
     @Slot(QImage)
     def update_image(self, image):
-        self.img_label1.setPixmap(QPixmap(QSize(1270, 720)).fromImage(image).scaled(1270, 720, Qt.AspectRatioMode.IgnoreAspectRatio, Qt.TransformationMode.SmoothTransformation))
+        self.img_label1.setPixmap(QPixmap.fromImage(image).scaled(self.video_size, Qt.AspectRatioMode.IgnoreAspectRatio, Qt.TransformationMode.SmoothTransformation))
 
 if __name__ == "__main__":
     app = QApplication([])
