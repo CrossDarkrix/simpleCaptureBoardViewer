0bd77561f86deddc4965c56fd9cc94401b1b28e9
CrossDarkrix <crosseddarkrix@gmail.com>
2025-10-06 03:17:53 +0900

    fix noise.

--- CaptureBoardViewer.py
+++ CaptureBoardViewer.py
@@ -1,66 +1,67 @@
 import ast
-import concurrent.futures
-import os
+import asyncio
+import multiprocessing
 import sys
-import time
-
 import cv2
 import pyaudio
 from PySide6.QtCore import Qt, Signal, Slot, QThread, QSize
 from PySide6.QtGui import QImage, QPixmap
 from PySide6.QtWidgets import QMainWindow, QLabel, QApplication
 
+_Playing = [True]
+
+def check_device(): # check index from "USB Capture Board"
+    audio = pyaudio.PyAudio()
+    for i in range(audio.get_device_count()):
+        data = ast.literal_eval('{}'.format(audio.get_device_info_by_index(i)).encode("utf-8", errors='ignore').decode("utf-8", errors='ignore'))
+        if data["hostApi"] == 2 and "USB3.0 Capture" in data["name"]:
+            return data["index"]
+
+stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
+                                     rate=96000,
+                                     channels=1,
+                                     input_device_index=check_device(),
+                                     input=True) # WebCam mic input.
+play = pyaudio.PyAudio().open(format=pyaudio.paInt16,
+                                   rate=96000, channels=1,
+                                   output_device_index=pyaudio.PyAudio().get_default_output_device_info()['index'],
+                                   output=True) # output to Speaker
+
+def _audio():
+    while _Playing[0]:
+        play.write(stream.read(1024))
+
 
 class VideoThread(QThread):
     change_pixmap_signal = Signal(QImage)
     playing = True
+    _Playing[0] = True
 
     def run(self):
-        def _audio():
-            def check_device():
-                audio = pyaudio.PyAudio()
-                for i in range(audio.get_device_count()):
-                    data = ast.literal_eval(
-                        '{}'.format(audio.get_device_info_by_index(i)).encode("utf-8", errors='ignore').decode("utf-8", errors='ignore'))
-                    if data["hostApi"] == 2 and "USB3.0 Capture" in data["name"]:
-                        return data["index"]
-            stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
-                                     rate=96000,
-                                     channels=1,
-                                     input_device_index=check_device(),
-                                     input=True)
-            play = pyaudio.PyAudio().open(format=pyaudio.paInt16,
-                                   rate=96000, channels=1,
-                                   output_device_index=pyaudio.PyAudio().get_default_output_device_info()['index'],
-                                   output=True
-                                   )
-            while self.playing:
-                concurrent.futures.ThreadPoolExecutor().submit(play.write, stream.read(7024))
-
-        def _video():
+        async def _video():
             cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-            cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-            cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 768)
+            cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1200)
+            cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 700)
             cap.set(cv2.CAP_PROP_FPS, 120)
             while self.playing:
                 ret, frame = cap.read()
                 if ret:
                     h, w, ch = frame.shape
                     bytesPerLine = ch * w
-                    self.change_pixmap_signal.emit(QImage(frame.data, w, h, bytesPerLine, QImage.Format.Format_BGR888).scaled(QSize(1280, 768), Qt.AspectRatioMode.IgnoreAspectRatio, Qt.TransformationMode.SmoothTransformation).convertToFormat(QImage.Format.Format_RGBA32FPx4_Premultiplied, Qt.ImageConversionFlag.NoOpaqueDetection))
+                    self.change_pixmap_signal.emit(QImage(frame.data, w, h, bytesPerLine, QImage.Format.Format_BGR888))
             cap.release()
-        concurrent.futures.ThreadPoolExecutor(os.cpu_count() * 999999999).submit(_audio)
-        concurrent.futures.ThreadPoolExecutor(os.cpu_count() * 999999999).submit(_video)
-        while self.playing:
-            time.sleep(1)
+            return
+        multiprocessing.Process(target=_audio, daemon=True).start() # run subprocess
+        asyncio.run(_video())
 
     def stop(self):
         self.playing = False
+        _Playing[0] = False
         self.wait()
 
 
 class Window(QMainWindow):
-    video_size = QSize(1280, 768)
+    video_size = QSize(1200, 700)
     def __init__(self):
         super().__init__()    
         self.initUI()
@@ -96,4 +97,5 @@ def main():
     sys.exit(app.exec())
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     main()
